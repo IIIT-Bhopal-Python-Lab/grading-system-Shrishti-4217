9a4aa07e16d9460c5bd08a334403957cbd39da35
Shrishti-4217 <shrishtimishra437@gmail.com>
2025-08-01 00:03:08 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,31 +1,13 @@
-    def grade(marks):  #function that takes input as marks and returns the grade
-    if int(marks)<=100 and int(marks)>=90:
-        return "A"
-    elif int(marks)<=89 and int(marks)>=75:
-        return "B"
-    elif int(marks)<=74 and int(marks)>=60:
-        return "C"
-    elif int(marks)<=59 and int(marks)>=40:
-        return "D"
-    elif int(marks)<40:
-        return "F"
-    
-    
-marks=input("Enter the marks:")   #Enter the marks
-while(marks!="EXIT"):   #run until EXIT
-    if(marks.isdecimal()):    #if the string is a number
-        if (int(marks)<0 or int(marks)>100):  #out of range condition
-            print("Invalid input")
-            marks=input("Enter the marks:") 
-        else:                                #if the marks is in the range
-            gd=grade(marks)        # function is called
-            print("Your grade is:",gd)
-            marks=input("Enter the marks:") 
-    else:
-        print("Invalid input")
-        marks=input("Enter the marks:") 
-
-
-
-    
-       
+mark = int(input())
+if 90 <= mark <= 100:
+    print("A")
+elif 75 <= mark<= 89:
+    print("B")
+elif 60 <= mark <= 74:
+    print("C")
+elif 40 <= mark <= 59:
+    print("D")
+elif 0 <= mark <= 39:
+    print("F")
+else:
+    print("Invalid mark")
